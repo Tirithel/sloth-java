f93c23accd13d9a72f776263c3f2e67471a6d896
Tirithel <colin.mtech@gmail.com>
2021-05-21 17:54:39 -0400

    Adding todays course work.

--- week-1/class/app/src/main/java/org/sloth/App.java
+++ week-1/class/app/src/main/java/org/sloth/App.java
@@ -4,14 +4,28 @@ package org.sloth;
  * Hello world!
  */
 public final class App {
-    private App() {
-    }
-
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int apples = 0;
+
+        int bobbysApples = 2;
+        int annsApples = 3;
+
+        int pearBush = 14;
+        int JohnSmith = 42;
+
+        double JohnSmithsWallet = 423.98;
+        double pearCost = 3.00;
+
+        apples = bobbysApples + annsApples;
+
+        int pearsHeCanAfford = ((int) (JohnSmithsWallet/pearCost));
+
+        System.out.println("Apples:\t" + apples);
+        System.out.println("Pears:\t" + (pearBush * JohnSmith));
+        System.out.println("John Smith can buy " + pearsHeCanAfford + " pears, with $" + (JohnSmithsWallet % pearCost) + " left over");
     }
 }
